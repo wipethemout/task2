5194405d0e82913472cee73ffa9b286953e1d0d7
stickowar <stickowar@gmail.com>
2020-11-22 17:28:42 +0500

    basic

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def draw_circle(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    ex.show()
+    sys.exit(app.exec_())
